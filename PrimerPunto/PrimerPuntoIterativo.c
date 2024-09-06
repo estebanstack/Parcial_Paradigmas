21ad5f637d286365ac409648af9525ff74f06e8d
estebanstack <130319203+estebanstack@users.noreply.github.com>
2024-09-06 15:10:29 -0500

    Update PrimerPuntoIterativo.c

--- PrimerPunto/PrimerPuntoIterativo.c
+++ PrimerPunto/PrimerPuntoIterativo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>  // -> libreria para medir el tiempo
 
 unsigned long long factorial_iterativo(int n) {
     unsigned long long fact = 1;
@@ -10,10 +11,24 @@ unsigned long long factorial_iterativo(int n) {
 
 int main() {
     int numero;
+    clock_t inicio, fin;  // Variables para medir el tiempo
+    double tiempo_total;
+
     printf("Introduce un número entero: ");
     scanf("%d", &numero);
+    // Inicia la medición del tiempo
+    inicio = clock();
+    
+    unsigned long long resultado = factorial_iterativo(numero);
 
-    printf("Factorial (iterativo) de %d es: %llu\n", numero, factorial_iterativo(numero));
+    // Finaliza la medición del tiempo
+    fin = clock();
+
+    // Calcula el tiempo total en segundos
+    tiempo_total = (double)(fin - inicio) / CLOCKS_PER_SEC;
+
+    printf("Factorial (iterativo) de %d es: %llu\n", numero, resultado);
+    printf("El programa tardó %.6f segundos en ejecutarse.\n", tiempo_total);
 
     return 0;
 }
