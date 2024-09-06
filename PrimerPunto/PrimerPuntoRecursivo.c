5030a0515e1c69a040b7e06535fc9aed70208223
estebanstack <130319203+estebanstack@users.noreply.github.com>
2024-09-06 15:12:32 -0500

    Update PrimerPuntoRecursivo.c

--- PrimerPunto/PrimerPuntoRecursivo.c
+++ PrimerPunto/PrimerPuntoRecursivo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>  
 
 unsigned long long factorial_recursivo(int n) {
     if (n == 0 || n == 1) {
@@ -10,10 +11,21 @@ unsigned long long factorial_recursivo(int n) {
 
 int main() {
     int numero;
+    clock_t inicio, fin; 
+    double tiempo_total;
+
     printf("Introduce un número entero: ");
     scanf("%d", &numero);
+    inicio = clock();
+    
+    unsigned long long resultado = factorial_recursivo(numero);
+    
+    fin = clock();
+    
+    tiempo_total = (double)(fin - inicio) / CLOCKS_PER_SEC;
 
-    printf("Factorial (recursivo) de %d es: ", numero, factorial_recursivo(numero));
+    printf("Factorial (recursivo) de %d es: %llu\n", numero, resultado);
+    printf("El programa tardó %.6f segundos en ejecutarse.\n", tiempo_total);
 
     return 0;
 }
